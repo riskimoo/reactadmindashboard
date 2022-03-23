15f4c75c404fbc1bc9ea94c24557154ae2d8cbc9
Muhammad Rizki Pratomo <64060806+riskimoo@users.noreply.github.com>
2022-03-23 11:02:37 +0700

    Add featured area

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,10 @@
+import './chart.scss'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+const Chart = () => {
+    return (
+        <div className='chart'>Chart</div>
+    )
+}
+
+export default Chart
