6a916a85ab384296da9095c13383b8d49e392a10
Muhammad Rizki Pratomo <64060806+riskimoo@users.noreply.github.com>
2022-03-24 16:13:24 +0700

    Add new page and add props to new page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
+import {userInputs, productInputs} from "./formSource"
 
 function App() {
   return (
@@ -20,12 +21,12 @@ function App() {
           <Route path="users">
             <Route index element={<List/>}/>
             <Route path=":userID" element={<Single/>}/>
-            <Route path="new" element={<New/>}/>
+            <Route path="new" element={<New inputs={userInputs} title ="Add new user"/>}/>
           </Route>
           <Route path="products">
             <Route index element={<List/>}/>
             <Route path=":productID" element={<Single/>}/>
-            <Route path="new" element={<New/>}/>
+            <Route path="new" element={<New inputs={productInputs} title="Add new product"/>}/>
           </Route>
         </Route>
       </Routes>
